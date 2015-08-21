f53dcdf6710ec17642292b8a07716b8cf6976a34
Diego Rueda <diego.rueda2@gmail.com>
2015-08-21 12:10:51 -0400

    example routes in laravel5

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Test
+Route::get('/probando/ruta', function(){
+	//código a ejecutar cuando se produzca esa ruta y el verbo 
+	return 'get';
+});
+
+Route::post('/probando/ruta', function(){
+	//código a ejecutar cuando se produzca esa rutay el verbo POST
+	return 'post';
+});
+
+Route::put('/probando/ruta', function(){
+	//código a ejecutar cuando se produzca esa rutay el verbo PUT
+	return 'put';
+});
+
+Route::delete('/probando/ruta', function(){
+	//código a ejecutar cuando se produzca esa rutay el verbo DELETE
+	return 'delete';
+});
+
+ /*Como puedes imaginar, existen casos en los que te puede interesar registrar una ruta que tenga validez con varios verbos, 
+ indicando una única función que los resuelva todos. Esto se puede conseguir mediante dos métodos distintos.*/
+
+Route::match(['get', 'post', 'put'], '/testing', function () {
+    echo  'Ruta testing para los verbos GET, POST, PUT';
+});
+
+//Por otra parte tenemos el método any, que es como un comodín, que sirve para cualquier tipo de verbo del HTTP. 
+
+Route::any("/cualquiercosa", function(){
+	echo 'La ruta /cualquiercosa asociada a cualquier verbo';
+});
